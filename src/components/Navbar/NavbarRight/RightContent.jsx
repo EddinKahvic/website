0e7b64933875c5303f6eac1bb1113b39d0b4cd0b
EddinKahvic <eddin.kahvic@hotmail.com>
2024-12-02 20:13:45 +0100

    Started developing the right content search input button form

--- src/components/Navbar/NavbarRight/RightContent.jsx
+++ src/components/Navbar/NavbarRight/RightContent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import MagnifyingGlass from '../../../icons/MagnifyingGlass'
 import './RightContent.css'
 
@@ -8,12 +8,14 @@ function RightContent() {
       <div className="Navbar-right-search">
         <form action="/">
           <div className="Navbar-right-search-input-container">
-            <input type="text" />
             <div className="Navbar-right-search-icon">
-              <button type="submit" aria-hidden="true">
-                <MagnifyingGlass />
-              </button>
+              <MagnifyingGlass />
+              <input type="submit"></input>
+              {/* <button type="reset">
+                <X />
+              </button> */}
             </div>
+            <input type="text" defaultValue="" />
           </div>
         </form>
       </div>
