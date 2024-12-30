0ac10df65b7fd4fd1438e05af879bbed20ff16ec
EddinKahvic <eddin.kahvic@hotmail.com>
2024-12-30 02:40:25 +0100

    Made search button responsive when pressing it

--- src/components/Navbar/NavbarRight/RightContent.jsx
+++ src/components/Navbar/NavbarRight/RightContent.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MagnifyingGlass from '../../../icons/MagnifyingGlass'
 import './RightContent.css'
 
 function RightContent() {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  //Toggles state values based on previous state
+  const toggleExpand = () => {
+    setIsExpanded((previous) => !previous)
+  }
+
   return (
     <div className="Navbar-right-content">
       <div className="Navbar-right-search">
         <form action="/">
-          <div className="Navbar-right-search-input-container">
+          <div
+            className="Navbar-right-search-input-container"
+            style={{
+              width: isExpanded ? '254px' : '28px',
+              transition: 'width 1s ease-out',
+            }}
+          >
             <div className="Navbar-right-search-icon">
-              <MagnifyingGlass />
-              <input type="submit"></input>
+              <button
+                type="button"
+                className="Navbar-right-search-button"
+                onClick={() => {
+                  toggleExpand()
+                }}
+              >
+                <MagnifyingGlass />
+              </button>
+
               {/* <button type="reset">
                 <X />
               </button> */}
             </div>
-            <input type="text" defaultValue="" />
+            <input
+              type="text"
+              defaultValue=""
+              style={{
+                width: isExpanded ? '100%' : '0%',
+                transition: 'width 1s ease-out',
+              }}
+            />
           </div>
         </form>
       </div>
